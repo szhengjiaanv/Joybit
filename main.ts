251009c12b224fde7318dca0f6bc7afbf98ed05f
szhengjiaanv <120627122+szhengjiaanv@users.noreply.github.com>
2023-01-09 16:38:19 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,6 @@ namespace Joybit {
  * TODO: Get Joystick XY value
  */
 
-
     export enum RockerXY {
         LeftRockerX = 0x1,
         LeftRockerY = 0x2,
@@ -20,6 +19,34 @@ namespace Joybit {
         RightRockerY = 0x4
     }
 
+    export enum Buttons {
+        //%block="C"
+        P12 = DAL.MICROBIT_ID_IO_P12,
+        //%block="D"
+        P13 = DAL.MICROBIT_ID_IO_P13,
+        //%block="Left Rocker Button"
+        P14 = DAL.MICROBIT_ID_IO_P14,
+        //%block="Right Rocker Button"
+        P15 = DAL.MICROBIT_ID_IO_P15
+    }
+
+    export enum ButtonAction {
+        //%block="Down"
+        Down = PulseValue.High,
+        //%block="Up"
+        Up = PulseValue.Low
+    }
+
+    //% weight=90
+    //% blockId=JoybitInitial block="Joystick Initialization"
+    export function JoybitInitial(): void {
+        pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+        pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+    }
+
+    //% weight=89
     //% blockId=FunbitMotorStart block="Rocker |%index"
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     export function JoybitRockerXY(index: RockerXY) :number {
@@ -30,5 +57,26 @@ namespace Joybit {
         pins.i2cWriteBuffer(ADDR, buf);
         v = pins.i2cReadNumber(ADDR, NumberFormat.UInt8BE);
         return v;
+    }
+
+    //% weight=87
+    //% blockId=onButtonEvent block="on button %button|is %event"
+    export function onButtonEvent(button: Buttons, event: ButtonAction, handler: Action): void {
+        pins.onPulsed(<number>button, <number>event, handler);
+    }
+
+    //% weight=86
+    //% blockId=getButton block="button %button is pressed"
+    export function getButton(button: Buttons): boolean {
+        let ret = false
+        if (pins.digitalReadPin(<number>button) == 0)
+        {
+            ret = true
+        }
+        else
+        {
+            ret = false
+        }
+        return ret
     }
 }
